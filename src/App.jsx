248ca6055835b00916e12f668b4c4b2e1d667187
Thomás Ledesma <98626681+Thomy-Ledesma@users.noreply.github.com>
2024-04-21 18:36:31 -0300

    practica 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
-import ToDoList from './components/ToDoList/ToDoList'
+import Table from './components/Table/Table';
 
 const App = () =>{
+  const netIncomes = [{brand: 'McDonalds', income: 1291283}, {brand: 'Burger King', income: 1927361}, {brand: 'KFC', income: 1098463}];
+  const netIncomesAverage = Math.round(netIncomes.reduce((total, restaurant) =>{return total += restaurant.income / netIncomes.length}, 0))
   return(
-    <div className='container'>
-      <ToDoList />
-    </div>
+    <>
+      <Table netIncomes={netIncomes}/>
+      <p>Average net income: ${netIncomesAverage}</p>
+    </>
   )
 }
 
